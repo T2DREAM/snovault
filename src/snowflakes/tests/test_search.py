47bfd6bd9f52382c065a14861f52224a72de81d8
Parul-Kudtarkar <parulbioinf@gmail.com>
2018-08-17 21:50:24 -0700

    exclude view test

--- src/snowflakes/tests/test_search.py
+++ src/snowflakes/tests/test_search.py
@@ -18,18 +18,6 @@ def test_search_view(workbook, testapp):
     assert '@graph' in res
 
 
-def test_report_view(workbook, testapp):
-    res = testapp.get('/report/?type=Lab').json
-    assert res['@type'] == ['Report']
-    assert res['@id'] == '/report/?type=Lab'
-    assert res['@context'] == '/terms/'
-    assert res['notification'] == 'Success'
-    assert res['title'] == 'Report'
-    assert res['total'] > 0
-    assert 'facets' in res
-    assert 'filters' in res
-    assert 'columns' in res
-    assert '@graph' in res
 
 # Unit tests
 
